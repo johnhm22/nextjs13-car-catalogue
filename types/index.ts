b02e5693c9be53159b560fe3e6ce8a1e70c9ac47
john <johnhowardmorgan@outlook.com>
2023-07-11 11:44:59 +0100

    Updating and adding type definitions

--- types/index.ts
+++ types/index.ts
@@ -1,8 +1,50 @@
-import { MouseEvent } from "react";
+import { MouseEvent } from 'react';
 
 export interface ICustomButton {
   title: string;
   containerStyles?: string;
   handleClick?: (e: MouseEvent<HTMLButtonElement>) => void;
-  btnType: "button" | "submit";
+  btnType?: 'button' | 'submit';
+  textStyles?: string;
+  rightIcon?: string;
+}
+
+export interface ICarData {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: 'a';
+  year: number;
+}
+
+export interface IFilterProps {
+  manufacturer: string;
+  year: number;
+  fuel: string;
+  limit: number;
+  model: string;
+}
+
+export interface IOptionProps {
+  title: string;
+  value: string;
+}
+
+export interface ICustomFilterProps {
+  title: string;
+  options: IOptionProps[];
+  setFilter: (arg: string | number) => void;
+}
+
+export interface IShowMoreProps {
+  pageNumber: number;
+  isNext: boolean;
+  setLimit: (arg: number) => void;
 }
