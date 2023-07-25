26c2e8a9efd6430bae72b2156620ea8d2a6e0f10
john <johnhowardmorgan@outlook.com>
2023-07-25 15:42:26 +0100

    Converting code to client side, adding useState and useEffect where api is called

--- app/page.tsx
+++ app/page.tsx
@@ -1,93 +1,76 @@
-'use client';
-
-import { useEffect, useState } from 'react';
 import Image from 'next/image';
 
-import { CustomFilter, Hero, SearchBar, CarCard, ShowMore } from '@/components';
+import {
+  CustomFilter,
+  Hero,
+  SearchBar,
+  CarCard,
+  ShowMore,
+  CarDetails,
+} from '@/components';
 import { fuels, yearsOfProduction } from '@/constants';
-import { CarState, ICarData } from '@/types';
+import { ICarData, IFilterProps } from '@/types';
 import { fetchCars } from '@/utils';
 
-export default function Home() {
-  const [allCars, setAllCars] = useState<CarState>([]);
-  const [loading, setLoading] = useState(false);
+interface IProps {
+  searchParams: IFilterProps;
+}
 
-  //search states
-  const [manufacturer, setManufacturer] = useState('');
-  const [model, setModel] = useState('');
+export default async function Home({ searchParams }: IProps) {
+  let allCars: ICarData[];
 
-  //filter states
-  const [fuel, setFuel] = useState('');
-  const [year, setYear] = useState(2022);
+  allCars = await fetchCars({
+    manufacturer: searchParams.manufacturer || '',
+    year: searchParams.year || 2022,
+    fuel: searchParams.fuel || '',
+    limit: searchParams.limit || 10,
+    model: searchParams.model || '',
+  });
 
-  //pagination states
-  const [limit, setLimit] = useState(10);
-
-  const getCars = async () => {
-    setLoading(true);
-    try {
-      const result = await fetchCars({
-        manufacturer: manufacturer.toLowerCase() || '',
-        year: year || 2022,
-        fuel: fuel.toLowerCase() || '',
-        limit: limit || 10,
-        model: model.toLowerCase() || '',
-      });
-      setAllCars(result);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    getCars();
-  }, [fuel, manufacturer, model, limit]);
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
     <main className="Hero">
       <Hero />
-      <div className="mt-12 padding-x padding-y max-width" id="discover">
+      <div className="padding-x padding-y max-width mt-12" id="discover">
         <div className="home__text-container">
           <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore the cars you might like</p>
         </div>
         <div className="home__filters">
-          <SearchBar setManufacturer={setManufacturer} setModel={setModel} />
+          <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter options={fuels} setFilter={setFuel} />
-            <CustomFilter options={yearsOfProduction} setFilter={setYear} />
+            <CustomFilter title="fuel" options={fuels} />
+            <CustomFilter title="year" options={yearsOfProduction} />
           </div>
         </div>
-        {allCars.length > 0 ? (
+        {!isDataEmpty ? (
           <section>
             <div className="home__cars-wrapper">
               {allCars?.map((car: ICarData, index) => (
                 <CarCard car={car} key={index} />
               ))}
             </div>
-            {loading && (
-              <div className="mt-16 w-full flex-center">
-                <Image
-                  src="public/loader.svg"
-                  alt="loader"
-                  width={50}
-                  height={50}
-                  className="object-contain"
-                />
-              </div>
-            )}
-            <ShowMore
+            <div className="flex-center w-full mt-16">
+              <Image
+                src="public/loader.svg"
+                alt="loader"
+                width={50}
+                height={50}
+                className="object-contain"
+              />
+            </div>
+
+            {/* <ShowMore
               pageNumber={limit / 10}
               isNext={limit > allCars.length}
               setLimit={setLimit}
-            />
+            /> */}
           </section>
         ) : (
           <>
             <div className="home__error-container">
-              <h2 className="text-black text-xl">Oops! No data available</h2>{' '}
+              <h2 className="text-xl text-black">Oops! No data available</h2>{' '}
               <p>{allCars?.message}</p>
             </div>
           </>
