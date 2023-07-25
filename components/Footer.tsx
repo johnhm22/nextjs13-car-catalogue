4f2b4c497e8d92ae84005e0354b59ff747db6aec
john <johnhowardmorgan@outlook.com>
2023-07-25 15:44:46 +0100

    Reordering of tailwind attributes following extension installation

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,75 +4,53 @@ import { footerLinks } from '@/constants';
 import Link from 'next/link';
 
 const Footer = () => {
-    return (
-        <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
-            <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
-                <div className="flex flex-col justify-start gap-6">
-                    <Image
-                        src="/public/logo.svg"
-                        alt="logo"
-                        width={118}
-                        height={18}
-                        className="object-contain"
-                    ></Image>
-                    <p className="text-base text-gray-700">
-                        Carhub 2023 <br />
-                        All rights reserved &copy;
-                    </p>
-                </div>
-                {/* Footer links */}
-                <div className="footer__links">
-                    {footerLinks.map((link) => (
-                        <div
-                            key={link.title}
-                            className="footer__link"
-                        >
-                            <h3 className="font-bold">
-                                {link.title}
-                            </h3>
-                            {link.links.map(
-                                (item) => (
-                                    <Link
-                                        href={
-                                            item.url
-                                        }
-                                        key={
-                                            item.title
-                                        }
-                                        className="text-gray-500"
-                                    >
-                                        {
-                                            item.title
-                                        }
-                                    </Link>
-                                )
-                            )}
-                        </div>
-                    ))}
-                </div>
+  return (
+    <footer className="text-black-100 flex flex-col mt-5 border-t border-gray-100">
+      <div className="max-md:flex-col sm:px-16 flex flex-wrap justify-between gap-5 px-6 py-10">
+        <div className="flex flex-col justify-start gap-6">
+          <Image
+            src="/public/logo.svg"
+            alt="logo"
+            width={118}
+            height={18}
+            className="object-contain"
+          ></Image>
+          <p className="text-base text-gray-700">
+            Carhub 2023 <br />
+            All rights reserved &copy;
+          </p>
+        </div>
+        {/* Footer links */}
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div key={link.title} className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link
+                  href={item.url}
+                  key={item.title}
+                  className="text-gray-500"
+                >
+                  {item.title}
+                </Link>
+              ))}
             </div>
-            <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
-                <p>
-                    @2023 Carhub. All Rights
-                    Reserved
-                </p>
-                <div className="footer__copyrights-link">
-                    <Link
-                        href="/"
-                        className="text-gray-500"
-                    >
-                        Privacy Policy
-                    </Link>
-                    <Link
-                        href="/"
-                        className="text-gray-500"
-                    >
-                        Terms of Use
-                    </Link>
-                </div>
-            </div>
-        </footer>
-    );
+          ))}
+        </div>
+      </div>
+      <div className="sm:px-16 flex flex-wrap items-center justify-between px-6 py-10 mt-10 border-t border-gray-100">
+        <p>@2023 Carhub. All Rights Reserved</p>
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms of Use
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
